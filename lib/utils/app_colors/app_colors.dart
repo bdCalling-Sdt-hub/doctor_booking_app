49b0496f9b078fcdcef0e0c76464384b55515265
MasumRaj <promotionbd321@gmail.com>
2024-08-03 17:30:22 +0600

    project setup is totaly completed

--- lib/utils/app_colors/app_colors.dart
+++ lib/utils/app_colors/app_colors.dart
@@ -5,9 +5,32 @@ class AppColors {
   static const Color background = Color(0xffFDFDFD);
   static const Color appBarBackground = Color(0xff050A1E);
   static const Color selectNavbarColor = Color(0xffE2E8FF);
-  static const Color white = Color(0xffFFFFFF);
   static const Color borderColor = Color(0xffE7E7E7);
   static const Color black = Color(0xff2C2C2C);
   static const Color normalBlack = Color(0xff929394);
+
+
+
+
+  static const Color textColor = Color(0xff374151);
+  static const Color darkActive = Color(0xff6D6D6D);
+  static const Color blackDarker = Color(0xff020202);
+  static const Color whiteNormal = Color(0xffF2F2F2);
+  static const Color blackNormal = Color(0xff050505);
+  static const Color grayNormal = Color(0xff363636);
+  static const Color whiteDarker = Color(0xff555555);
+  static const Color whiteNormalHover = Color(0xffDADADA);
+  static const Color whiteLightActive = Color(0xffFBFBFB);
+  static const Color eyeColor = Color(0xffB2BCC9);
+  static const Color bluNormalHover = Color(0xff015BE3);
+  static const Color blackLight = Color(0xffE6E6E6);
+  static const Color whiteDarkHover = Color(0xff919191);
+  static const Color grayLightHover = Color(0xffE1E1E1);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color red = Color(0xffCC2020);
+  static const Color green = Color(0xff7CC84E);
+
+
+
 
 }
