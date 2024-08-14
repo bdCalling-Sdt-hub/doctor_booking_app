e3411d2430481c570af31f60db6c7f4273623d36
jahid0002 <jhjony0009@gmail.com>
2024-08-14 18:15:52 +0600

    fix issues

--- lib/utils/app_images/app_images.dart
+++ lib/utils/app_images/app_images.dart
@@ -4,9 +4,9 @@ class AppImages {
   ///====================== onboard =========================
 
   static const String accessLocation = "$basePath/accessLocation.png";
-  static const String onboardOne = "$basePath/onboard_one.png";
+  static const String onboardOne = "$basePath/onboard_one1.png";
   static const String onBoardTwo = "$basePath/onboard_two.png";
-  static const String onBoardThree = "$basePath/onboard_three.jpg";
+  static const String onBoardThree = "$basePath/onboard_three3.png";
   static const String logo = "$basePath/logo.png";
   static const String ilera = "$basePath/ilera.png";
   static const String bannerOne = "$basePath/banner_one.png";
