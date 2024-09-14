bec6fba4da6a3061ccb89fdf0744671e991c5675
rafsanopi <rafsan3@dti.ac>
2024-09-14 11:41:01 +0600

    Merge

--- lib/view/widgets/video_call/video_call.dart
+++ lib/view/widgets/video_call/video_call.dart
@@ -19,7 +19,7 @@ class AudioVideoCall extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ZegoUIKitPrebuiltCall(
-      events: ZegoUIKitPrebuiltCallEvents(),
+        events: ZegoUIKitPrebuiltCallEvents(),
         onDispose: () {
           print("---------- Dispose");
         },
